fa6659f1d2710d9d4686245e8d3e9ad7acbaa44c
Pekkakost <85934700+Pekkakost@users.noreply.github.com>
2023-09-25 18:45:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
+
 api_key = "4668a70e6c354de8a9e5c66c5edf6816"
-url = "https://newsapi.org/v2/everything?q=tesla& \
-       from=2023-08-25&sortBy=publishedAt&apiKey=" \
-       "4668a70e6c354de8a9e5c66c5edf6816"
+url =   "https://newsapi.org/v2/everything?" \
+        f"q={topic}&" \
+        "sortBy=publishedAt&" \
+        "apiKey=4668a70e6c354de8a9e5c66c5edf6816&" \
+        "language=en"
+
 # make request
 request = requests.get(url)
 
 #get a dictionery with data
 content=request.json()
-print(content['articles'])
-
-print(len(content["articles"]))
-
 #access to article with title ans description
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + article["description"] +2*"\n"
+        body = "Subject:Today's news" \
+            +"\n" + body + article["title"] + "\n"\
+            + article["description"] + "\n" \
+            + article["url"]+2*"\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
